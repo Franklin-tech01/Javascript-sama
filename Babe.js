d254084be44366254633ccd1d6586a475855e542
Franklin-tech01 <franklinmike2021@gmail.com>
2024-01-08 10:05:30 +0100

    Hope took be back to python

---
+++ Babe.js
@@ -0,0 +1,13 @@
+function addSum(a, b) {
+    console.log(a + b)
+}
+
+addSum(2, 7)
+
+function calculate(num1, num2)
+{
+    console.log(num1 + num2);
+}
+
+
+calculate(2, 5)
---
+++ Hope.py
@@ -0,0 +1,12 @@
+savings = 100;
+
+#First we create our variables 
+monthly_savings = 10;
+num_months = 4;
+
+#we then mutply monthly_savings and num_months and save it to a variable called new_savings
+new_savings = monthly_savings * num_months;
+
+#add new_savings to that first savings you declared and store it in a variable called total_savings
+total_savings = new_savings + savings
+print(total_savings)
